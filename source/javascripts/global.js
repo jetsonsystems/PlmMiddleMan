026da205a9e74e9e609e66515950a5cf67fb076c
Ahmad Varoqua <ahmad.varoqua@gmail.com>
2013-03-04 00:30:02 -0800

    PPM > Dev > Flexbox > Hamburger animation

--- source/javascripts/global.js
+++ source/javascripts/global.js
@@ -1,3 +1,13 @@
 $(document).ready(function(){
 
+  /*
+  $("#hamburger-animate").click(function(){
+    console.log('Hamburger clicked.');
+    $("#hamburger").show().animate({width: '240px', avoidTransforms: true, useTranslate3d: true}, 'fast');
+  });
+  */
+
+  // $("#hamburger-animate").pageslide({ direction: "left", modal: true });
+
+  $(".second").pageslide({ direction: "right", modal: false });
 });
