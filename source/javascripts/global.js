fb1558fdda647e4c7d2e177f2dbe5a72719c685c
James Watson <jameswatsonv@gmail.com>
2013-04-28 22:23:15 -0700

    Updates

--- source/javascripts/global.js
+++ source/javascripts/global.js
@@ -3,4 +3,7 @@ $(document).ready(function(){
   // Activate the hamburger slider
   $(".hamburger-button").pageslide({ direction: "right", modal: false });
 
+    // Hack for Entypo font-icon visual bug on windows
+    $(".icon-button a").add("header a").add("nav a").add(".icon").attr('style', 'margin-top:-10px;padding-bottom:10px;');
+
 });
